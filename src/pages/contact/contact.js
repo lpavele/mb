a64187d3443cab2d43b7d2e3deacc3ccc52d31d7
Pavel Luchenok <pavel_l@logic-way.com>
2017-10-12 18:25:37 +0300

    Add yandex map

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,1 +1,14 @@
 import '../base';
+
+ymaps.ready(init);
+var myMap, myPlacemark;
+
+function init(){
+    myMap = new ymaps.Map("map", {
+        center: [53.821372, 27.519022],
+        zoom: 13
+    });
+    myPlacemark = new ymaps.Placemark([53.816852, 27.502800],{hintContent: 'МодульБетон'}, {preset: 'islands#blackDotIcon'});
+
+    myMap.geoObjects.add(myPlacemark);
+}
