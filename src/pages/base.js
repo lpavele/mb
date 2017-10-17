b32c496034bf374b577872dc51f16c9c05b6480e
lpavele <pasha1993xfenikcx>
2017-10-17 23:05:55 +0300

    Add email support

--- src/pages/base.js
+++ src/pages/base.js
@@ -2,3 +2,15 @@ import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import 'font-awesome/css/font-awesome.min.css';
 import './base.scss';
 import 'bootstrap';
+
+// (function(){
+//     emailjs.init('user_6A1ygXmpXIXrBGOTMXne6');
+//     var server_id = 'yandex_id';
+//     var template_id = 'request_template_id';
+//     emailjs.send(server_id, template_id,{phone: "+375 29 000 00 00"})
+//         .then(function(response) {
+//             console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
+//         }, function(err) {
+//             console.log("FAILED. error=", err);
+//         });
+// })();
